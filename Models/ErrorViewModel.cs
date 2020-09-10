110df8acf78a6672b1807322943ca9f178cda697
iiii110270 <71020318+iiii110270@users.noreply.github.com>
2020-09-10 14:44:35 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PushGithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
